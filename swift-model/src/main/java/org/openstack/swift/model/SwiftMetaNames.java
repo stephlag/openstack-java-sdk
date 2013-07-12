7e22a97416c1398283997877943b18c3435c2f0d
stefanl2@yahoo.fr <stefanl2@yahoo.fr>
2013-07-12 18:24:55 +0200

    Add a metadata for content length

--- swift-model/src/main/java/org/openstack/swift/model/SwiftMetaNames.java
+++ swift-model/src/main/java/org/openstack/swift/model/SwiftMetaNames.java
@@ -5,5 +5,8 @@ public class SwiftMetaNames {
     public final static String APP_DIR = "application/directory";
     
     public final static String CONTENT_TYPE = "content_type";
+    
     public final static String LAST_MODIFIED = "last_modified";
+
+    public final static String CONTENT_LENGTH = "Content-Length";
 }
